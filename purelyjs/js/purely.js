0be170ff10c0b587f9ab03bc477d2a14282dd483
Martin Matusiak <numerodix@gmail.com>
2014-10-19 11:31:56 +0200

    follow standard convention of returning an object

--- purelyjs/js/purely.js
+++ purelyjs/js/purely.js
@@ -1,5 +1,10 @@
-var purely = {
-    throwAssertionError: function(msg) {
+var purely = (function() {
+
+    /*
+     * Helper functions
+     */
+
+    var throwAssertionError = function(msg) {
         // throws TypeError as there isn't a built-in assertion error type
         throw TypeError('AssertionError: ' + msg);
 
@@ -10,9 +15,9 @@ var purely = {
                 message: 'AssertionError: ' + msg
             };
         */
-    },
+    };
 
-    contains: function(item, arr) {
+    var contains = function(item, arr) {
         var found = false;
 
         for (var i = 0; i < arr.length; i++) {
@@ -23,99 +28,108 @@ var purely = {
         }
 
         return found;
-    },
+    };
 
 
-    assertEqual: function(x, y) {
-        // jshint -W018
-        if (!(x === y)) {
-            purely.throwAssertionError((x && x.toString()) + ' === ' + (y && y.toString()));
-        }
-    },
+    /*
+     * Public API
+     */
 
-    assertNotEqual: function(x, y) {
-        // jshint -W018
-        if (!(x !== y)) {
-            purely.throwAssertionError((x && x.toString()) + ' !== ' + (y && y.toString()));
-        }
-    },
+    return {
 
+        assertEqual: function(x, y) {
+            // jshint -W018
+            if (!(x === y)) {
+                throwAssertionError((x && x.toString()) + ' === ' + (y && y.toString()));
+            }
+        },
 
-    assertGreater: function(x, y) {
-        // jshint -W018
-        if (!(x > y)) {
-            purely.throwAssertionError((x && x.toString()) + ' > ' + (y && y.toString()));
-        }
-    },
-
-    assertGreaterEqual: function(x, y) {
-        // jshint -W018
-        if (!(x >= y)) {
-            purely.throwAssertionError((x && x.toString()) + ' >= ' + (y && y.toString()));
-        }
-    },
-
-    assertLess: function(x, y) {
-        // jshint -W018
-        if (!(x < y)) {
-            purely.throwAssertionError((x && x.toString()) + ' < ' + (y && y.toString()));
-        }
-    },
-
-    assertLessEqual: function(x, y) {
-        // jshint -W018
-        if (!(x <= y)) {
-            purely.throwAssertionError((x && x.toString()) + ' <= ' + (y && y.toString()));
-        }
-    },
+        assertNotEqual: function(x, y) {
+            // jshint -W018
+            if (!(x !== y)) {
+                throwAssertionError((x && x.toString()) + ' !== ' + (y && y.toString()));
+            }
+        },
 
 
-    assertIn: function(item, arr) {
-        var found = purely.contains(item, arr);
+        assertGreater: function(x, y) {
+            // jshint -W018
+            if (!(x > y)) {
+                throwAssertionError((x && x.toString()) + ' > ' + (y && y.toString()));
+            }
+        },
 
-        if (!found) {
-            purely.throwAssertionError((item && item.toString()) + ' not in ' + (arr && arr.toString()));
-        }
-    },
+        assertGreaterEqual: function(x, y) {
+            // jshint -W018
+            if (!(x >= y)) {
+                throwAssertionError((x && x.toString()) + ' >= ' + (y && y.toString()));
+            }
+        },
 
-    assertNotIn: function(item, arr) {
-        var found = purely.contains(item, arr);
+        assertLess: function(x, y) {
+            // jshint -W018
+            if (!(x < y)) {
+                throwAssertionError((x && x.toString()) + ' < ' + (y && y.toString()));
+            }
+        },
 
-        if (found) {
-            purely.throwAssertionError((item && item.toString()) + ' in ' + (arr && arr.toString()));
-        }
-    },
+        assertLessEqual: function(x, y) {
+            // jshint -W018
+            if (!(x <= y)) {
+                throwAssertionError((x && x.toString()) + ' <= ' + (y && y.toString()));
+            }
+        },
 
 
-    assertRaises: function(exceptionName, func) {
-        var thrown = false;
+        assertIn: function(item, arr) {
+            var found = contains(item, arr);
 
-        try {
-            func();
-        } catch (e) {
-            if (e.name === exceptionName) {
-                thrown = true;
+            if (!found) {
+                throwAssertionError((item && item.toString()) + ' not in ' + (arr && arr.toString()));
+            }
+        },
+
+        assertNotIn: function(item, arr) {
+            var found = contains(item, arr);
+
+            if (found) {
+                throwAssertionError((item && item.toString()) + ' in ' + (arr && arr.toString()));
+            }
+        },
+
+
+        assertRaises: function(exceptionName, func) {
+            var thrown = false;
+
+            try {
+                func();
+            } catch (e) {
+                if (e.name === exceptionName) {
+                    thrown = true;
+                }
+            }
+
+            if (!thrown) {
+                throwAssertionError(exceptionName + ' not thrown');
+            }
+        },
+
+        assertRaisesAssertion: function(func) {
+            var thrown = false;
+
+            try {
+                func();
+            } catch (e) {
+                if ((e.name === 'TypeError') && (/^AssertionError: /.test(e.message))) {
+                    thrown = true;
+                }
+            }
+
+            if (!thrown) {
+                throwAssertionError('AssertionError not thrown');
             }
         }
 
-        if (!thrown) {
-            purely.throwAssertionError(exceptionName + ' not thrown');
-        }
-    },
+    };
 
-    assertRaisesAssertion: function(func) {
-        var thrown = false;
-
-        try {
-            func();
-        } catch (e) {
-            if ((e.name === 'TypeError') && (/^AssertionError: /.test(e.message))) {
-                thrown = true;
-            }
-        }
-
-        if (!thrown) {
-            purely.throwAssertionError('AssertionError not thrown');
-        }
-    }
-};
+})();
